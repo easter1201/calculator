8afaf3023e9d8fb184dfd737d7abcf27a2855f8a
easter1201 <kite1201kr@gmail.com>
2025-04-16 17:20:08 +0900

    Calculator 내 컬렉션 필드 설정, 연산결과 저장 구현

--- src/Lv2/Calculator.java
+++ src/Lv2/Calculator.java
@@ -1,22 +1,32 @@
 package Lv2;
 
+import java.util.*;
+
 public class Calculator {
+    ArrayList<String> record = new ArrayList<>();
+    String input = "";
+
     public long calculate(long x, long y, char operate){
         long result = 0;
         switch(operate){
             case '+':
                 result = x + y;
+                input = x + " + " + y + " = " + result;
                 break;
             case '-':
                 result = x - y;
+                input = x + " - " + y + " = " + result;
                 break;
             case '*':
                 result = x * y;
+                input = x + " * " + y + " = " + result;
                 break;
             case '/':
                 result = x / y;
+                input = x + " / " + y + " = " + result;
                 break;
         }
+        record.add(input);
         return result;
     }
 }
