18104b22225ce4812b135d199f9c4fb99114dcf2
easter1201 <kite1201kr@gmail.com>
2025-04-16 13:36:15 +0900

    사ì입력ë방식 변경 및 나눗셈 예외처리

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Calculator");
-        System.out.println("양의 정수 2개를 순차적으로 입력하세요.");
-
+        System.out.println("0 이상의 정수 1개를 입력하세요.");
         long x = sc.nextLong();
+        System.out.println("'+', '-', '*', '/' 중 하나의 연산자를 입력하세요.");
         char operate = sc.next().charAt(0);
+        System.out.println("0 이상의 정수 1개를 입력하세요.");
         long y = sc.nextLong();
-
-        switch(operate){
-            case '+':
-                System.out.println(x + y);
-                break;
-            case '-':
-                System.out.println(x - y);
-                break;
-            case '*':
-                System.out.println(x * y);
-                break;
-            case '/':
-                System.out.println(x / y);
-                break;
+        if(operate == '/' && y == 0) {
+            System.out.println("0은 분모가 될 수 없습니다.");
+        }
+        else{
+            switch(operate){
+                case '+':
+                    System.out.println("연산 결과: " + (x + y));
+                    break;
+                case '-':
+                    System.out.println("연산 결과: " + (x - y));
+                    break;
+                case '*':
+                    System.out.println("연산 결과: " + (x * y));
+                    break;
+                case '/':
+                    System.out.println("연산 결과: " + (x / y));
+                    break;
+            }
         }
     }
 }
