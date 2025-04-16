253e1818821cc9e7cbbf2f84a20cb8bae80bd9c3
easter1201 <kite1201kr@gmail.com>
2025-04-16 13:10:22 +0900

    ë기본 사칙연산 구현

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Calculator");
+        System.out.println("양의 정수 2개를 순차적으로 입력하세요.");
 
+        long x = sc.nextLong();
+        char operate = sc.next().charAt(0);
+        long y = sc.nextLong();
+
+        switch(operate){
+            case '+':
+                System.out.println(x + y);
+                break;
+            case '-':
+                System.out.println(x - y);
+                break;
+            case '*':
+                System.out.println(x * y);
+                break;
+            case '/':
+                System.out.println(x / y);
+                break;
+        }
     }
 }
