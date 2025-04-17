e1d436bdb69cee1a1eb91902679c530e5903d639
easter1201 <kite1201kr@gmail.com>
2025-04-17 10:06:33 +0900

    기능 일부 Display 메소드로 분리

--- src/Lv2/App.java
+++ src/Lv2/App.java
@@ -1,13 +1,38 @@
 package Lv2;
 
 import java.util.*;
+
+
 public class App {
+    public static boolean Display(Scanner sc,Calculator calc){ //결과 이후 출력
+        while(true){
+            System.out.println("종료를 원한다면 'exit'을, 기록을 보고싶다면 'record'를, 기록 삭제를 원한다면 'delete'를, 추가 계산을 원한다면 아무 키나 입력하세요.");
+            String order = sc.next();
+
+            if(order.equals("exit")){ //계산기 종료
+                System.out.println("계산기가 종료됩니다.");
+                return false;
+            }
+            else if(order.equals("record")){ //getter 사용 - 기록 확인
+                String[] record = calc.getRecord();
+                for(int i = 0; i < record.length; i++){
+                    System.out.println("Record " + (i + 1) + " : " + record[i]);
+                }
+            }
+            else if(order.equals("delete")){ //삭제 메소드 활용
+                calc.deleteRecord();
+                System.out.println("최근 기록 1개가 제거되었습니다.");
+            }
+            else return true;
+        }
+    }
     public static void main(String[] args){
         Calculator calc = new Calculator(); //연산 클래스 호출
         Scanner sc = new Scanner(System.in); //스캐너 호출
+        boolean flag = true;
 
         System.out.println("Calculator");
-        while(true) { //연산 반복
+        while(flag) { //연산 반복
             System.out.println("0 이상의 정수 1개를 입력하세요.");
             try { //정수가 아닌 다른 수 입력 시 예외처리
                 long x = sc.nextLong();
@@ -24,23 +49,8 @@ public class App {
                 }
                 else System.out.println("올바른 연산자를 입력해주세요.");
 
-                System.out.println("종료를 원한다면 'exit'을, 기록을 보고싶다면 'record'를, 추가 계산을 원한다면 아무 키나 입력하세요.");
-                String order = sc.next();
+                flag = Display(sc, calc); //후속 행동 위한 출력
 
-                if(order.equals("exit")){ //exit 입력시 탈출
-                    System.out.println("계산기가 종료됩니다.");
-                    break;
-                }
-                else if(order.equals("record")){ //getter 사용-기록 출력
-                    String[] record = calc.getRecord();
-                    for(int i = 0; i < record.length; i++){
-                        System.out.println("Record " + (i + 1) + " : " + record[i]);
-                    }
-                }
-                else if(order.equals("delete")){ //setter 사용-제거 기능
-                    calc.deleteRecord();
-                    System.out.println("최근 기록 1개가 제거되었습니다.");
-                }
             } catch(InputMismatchException e){ //잘못 입력 시 에러 메시지 출력, 초기화
                 System.out.println("잘못된 입력입니다.");
                 sc = new Scanner(System.in);
